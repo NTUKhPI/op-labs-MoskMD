138b682a17528e6be24207ea464c3e62ae3a5360
MoskMD <143921864+MoskMD@users.noreply.github.com>
2023-10-02 13:34:03 +0300

    Add files via upload

---
+++ Term1/Lab1_1/N4.1/N4.1/N4.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//Д.зм
+int main() {
+    int a = 5;
+    int b = 10;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
+
