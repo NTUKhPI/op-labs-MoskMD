ffa050cba10d0ace375f5e0ca238ba7152caee7a
MoskMD <143921864+MoskMD@users.noreply.github.com>
2023-10-02 13:33:17 +0300

    Add files via upload

---
+++ Term1/Lab1_1/N3/N3/N3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double x = 1.0;
+
+    x = x * x;  
+    x = x * x;  
+    x = x * x;  
+
+    std::cout << "8 measurement of number : " << x << std::endl;
+
+    return 0;
+}
